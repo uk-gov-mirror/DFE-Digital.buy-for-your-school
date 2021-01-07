de7c0d10a9f3970201a7f820b7de960b41f84fb9
Tom Hipkin <tomhipkin@gmail.com>
2021-01-07 11:57:33 +0000

    SPIKE: Render spec with Liquid template

--- app/controllers/journeys_controller.rb
+++ app/controllers/journeys_controller.rb
@@ -11,6 +11,15 @@ class JourneysController < ApplicationController
       steps: [:radio_answer, :short_text_answer, :long_text_answer]
     ).find(journey_id)
     @steps = @journey.steps.map { |step| StepPresenter.new(step) }
+
+    @answers = @steps.each_with_object({}) { |step, hash|
+      hash["answer_#{step.contentful_id}"] = step.answer.response.to_s
+    }
+
+    source = File.open("lib/specifications/catering.liquid").read
+    @template = Liquid::Template.parse(
+      source, error_mode: :strict
+    ) # Parses and compiles the template
   end
 
   private
